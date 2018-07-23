5aeebee78452b26c4c680f063a31dc99a89ac25e
Unknown <nicolas.ballu91@gmail.com>
2018-07-24 01:10:18 +0200

    index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-<<<<<<< HEAD
 //function de log
-=======
->>>>>>> master
 bot.on('ready', function() {
     bot.user.setGame("En cours de développement &help");
     console.log("Connected");
