693006138140e0a3ce680545b2058470ed82b2f0
Regio0 <pourlefun84@hotmail.fr>
2018-07-24 01:00:11 +0200

    test

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,8 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("&")
-
 bot.on('ready', function() {
     bot.user.setGame("En cours de développement &help");
     console.log("Connected");
