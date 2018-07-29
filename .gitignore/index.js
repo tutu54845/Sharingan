cdce16bcf4fb5f59cd15f5fb0ea5f7a000dc73a3
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 22:34:31 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Client = new Discord.Client();
 
-var prefix = ("&")
+var prefix = "&";
 
 Client.on('ready', function() {
     Client.user.setGame("😎Command : &help😎");
