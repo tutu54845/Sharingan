2a9ab306bfc628c4de7544930c69507520a41b0a
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 23:48:03 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,7 +21,36 @@ Client.on('message', message => {
     if (message.content === "sava"){
         message.reply("Tranquille et toi mon bro)");
         console.log("Le bot répond à ta question");
-    }
-});
+    });
+
+    Client.on('message', message => {
+
+  if (message.content.startsWith('!play')) {
+    // On récupère le premier channel audio du serveur
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    // On récupère les arguments de la commande 
+    // il faudrait utiliser une expression régulière pour valider le lien youtube
+    let args = message.content.split(' ')
+    // On rejoint le channel audio
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        // On démarre un stream à partir de la vidéo youtube
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        // On envoie le stream au channel audio
+        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+ });
         
 Client.login(process.env.BOT_TOKEN);
