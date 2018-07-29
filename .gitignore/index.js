3d4dc13ef57d812e74a56579213d80b51941f357
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 23:07:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,18 +1,1 @@
-const Discord = require('discord.js');
-const Client = new Discord.Client();
 
-var prefix = "&";
-
-Client.on('ready', function() {
-    Client.user.setGame("😎Command : &help😎");
-    console.log("Connected");
-});
-
-Client.on('message', message => {
-    if (message.content === "salut"){
-        message.reply("Wesh ma couille)");
-        console.log("Commande Salut effetué");
-    }
-});   
-        
-Client.login(process.env.BOT_TOKEN);
