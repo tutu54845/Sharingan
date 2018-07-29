286b7e509ecb42afa8719a7cd1a096a243d00105
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 21:48:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,7 @@
-// INDEX.JS //
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var prefix = ("&")
-
 client.on('ready', () => {
-    client.user.setGame("😎Command : &help😎");
     console.log('Connected!');
 });
 
@@ -19,9 +14,6 @@ client.on('message', message => {
         message.reply("Tranquille et toi mon bro");
         console.log("Le bot répond à ta question");
     }
-    if(message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -&help"
-    }
- });
+});
 
 client.login(process.env.BOT_TOKEN);
