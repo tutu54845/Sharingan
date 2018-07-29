1202455cb2ea7e50aeb74b7e4e5d189a45ee3aef
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 22:28:24 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,26 +1,27 @@
 const Discord = require('discord.js');
+const Client = new Discord.Client();
 
-const client = new Discord.Client();
+var prefix = ("&")
 
-var prefix = "&";
-
-client.on('ready', () => {
-    client.user.setGame("😎Command : &help😎");
-    console.log('Connected!');
+Client.on('ready', function() {
+    Client.user.setGame("😎Command : &help😎");
+    console.log("Connected");
 });
 
-client.on('message', message => {
-    if(message.content === "salut"){
-        message.reply("Wesh ma couille");
-        console.log("Le bot dit Salut");
+Client.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n - &help");
     }
-    if(message.content === "sava"){
-        message.reply("Tranquille et toi mon bro");
+    
+    if (message.content === "salut"){
+        message.reply("Wesh ma couille)");
+        console.log("Commande Salut effetué");
+    }
+
+    if (message.content === "sava"){
+        message.reply("Tranquille et toi mon bro)");
         console.log("Le bot répond à ta question");
     }
-     if(message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -&help"
-    }
 });
-
-client.login(process.env.BOT_TOKEN);
+        
+Client.login("process.env.BOT_TOKEN");
