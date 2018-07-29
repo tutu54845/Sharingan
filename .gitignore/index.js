0b204b05917016826f3031886084171599044ad6
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 21:14:07 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -19,16 +19,17 @@ client.on('message', message => {
         message.reply("Tranquille et toi mon bro");
         console.log("Le bot répond à ta question");
     }
+    
     if(message.content === prefix + "help"){
-    var help_embed = new Discord.RichEmed()
-    .setColor("#40A497")
-    .setTitle("Voici mes commandes d'aide")
-    .setDescription("Je suis un bot voici mes commandes disponible")
-    .addField("&help", "Affiche les commandes !"
-    .addField("Bonjour", "Le bot répond !")
-    .setFooter("Menu d'aide")
-    message.channel.sendMessage(help_embed);
-    console.log("Un utilisteur a effectué la commade d'aide !")
+       var help_embed = new Discord.RichEmed()
+       .setColor("#40A497")
+       .setTitle("Voici mes commandes d'aide")
+       .setDescription("Je suis un bot voici mes commandes disponible")
+       .addField("&help", "Affiche les commandes !"
+       .addField("Bonjour", "Le bot répond !")
+       .setFooter("Menu d'aide")
+       message.channel.sendMessage(help_embed);
+       console.log("Un utilisteur a effectué la commade d'aide !")
     }
  });
 
