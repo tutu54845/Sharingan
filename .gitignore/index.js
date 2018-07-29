6f7d343769cf2a9abf5788e388428e1660c29e19
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-07-29 22:43:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,10 +9,6 @@ Client.on('ready', function() {
 });
 
 Client.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n - &help");
-    }
-    
     if (message.content === "salut"){
         message.reply("Wesh ma couille)");
         console.log("Commande Salut effetué");
