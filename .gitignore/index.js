4f17693c3dee48efe7ce15cdd6e8409b04511f21
Unknown <nicolas.ballu91@gmail.com>
2018-07-30 19:44:48 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -9,19 +9,20 @@ Client.on('ready', function() {
 });
 
 Client.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n - &help");
-    }
-    
-    if (message.content === "salut"){
-        message.reply("Wesh ma couille)");
-        console.log("Commande Salut effetué");
-    }
-
-    if (message.content === "sava"){
-        message.reply("Tranquille et toi mon bro)");
-        console.log("Le bot répond à ta question");
-    }
+  if (message.content === prefix + "help"){
+      message.channel.sendMessage("Liste des commandes: \n - &help \n - &creatediscord \n - &play");
+  }
+  if (message.content === prefix + "creatediscord"){
+      message.channel.sendMessage("discord généré");
+  }
+  if (message.content === "salut"){
+      message.reply("Wesh ma couille");
+      console.log("Commande Salut effetué");
+  }
+  if (message.content === "sava"){
+      message.reply("Tranquille et toi mon bro");
+      console.log("Le bot répond à ta question");
+  }
 });
-        
+
 Client.login(process.env.BOT_TOKEN);
