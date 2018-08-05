9630acca3a2d7a5d6fa905beece80da259b9c1e5
Regio0 <41551641+tutu54845@users.noreply.github.com>
2018-08-06 00:22:45 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,12 +4,12 @@ const Client = new Discord.Client();
 var prefix = "&";
 
 Client.on('ready', function() {
-    Client.user.setGame("&help");
+    Client.user.setGame("&help|Développement en cours!");
     console.log("Connected");
 });
 
 Client.on('message', message => {
-  if (message.content === prefix + "&help|Développement en cours!"){
+  if (message.content === prefix + ""){
       message.channel.sendMessage("Liste des commandes: \n - &help \n - &creatediscord \n - &play");
   }
   if (message.content === prefix + "creatediscord"){
