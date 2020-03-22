bc2853cb3ba64b9ed9eb46ca8a08cf573f970de0
Regio0 <41551641+tutu54845@users.noreply.github.com>
2020-03-22 22:00:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+
 const Client = new Discord.Client();
 
 var prefix = "&";
@@ -6,21 +7,29 @@ var prefix = "&";
 Client.on('ready', function() {
     Client.user.setGame("Développement en cours !!!");
     console.log("Connected");
+
 });
 
-Client.on('message', message => {
-    if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n - &help");
-    }
-    
-    if (message.content === "salut"){
-        message.reply("Wesh ma couille)");
-        console.log("Commande Salut");
+client.on('message', async message => { 
+
+    if(message.content === "yo"){
+        message.reply("Salut");
+        console.log('Le bot dit bonjour');
     }
 
-    if (message.content === "sava"){
-        message.reply("Tranquille et toi mon bro)");
-        console.log("Le bot répond a  ta question");
+    if(message.content === prefix + "help") {
+      var aide_embed = new Discord.RichEmbed()
+      .setColor('#ff0033')
+      .setTitle(`*Sharingan - Page d'aide !!!*`)
+      .setDescription(`Description`)
+      .setThumbnail('https://i.imgur.com/57KGUTQ.png')
+      .addField(":tools: Modération", "`mod`,`kick`,`ban`,`mute`,`unmute`,`clear`,")
+      .addField(":tada: Fun", "Fais `fun` pour voir mes commandes d'animation !")
+      .addField(":headphones: Musique", "`play`, `pause`, `skip`, `stop`")
+      .addField(":movie_camera: Twitch", "Fais `twit` pour voir mes commandes de steam !")
+      .setFooter("Sharingan V1.00.0")
+      .setTimestamp()
+      message.channel.send(aide_embed);
     }
 });
         
